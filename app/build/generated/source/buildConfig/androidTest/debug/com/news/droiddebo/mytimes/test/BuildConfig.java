39f0148798618486c411405cd1cb57e15c1f7c51
chhabriamuskan31 <muskanchhabria2002@gmail.com>
2024-04-22 23:41:05 +0530

    final project

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/news/droiddebo/mytimes/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.news.droiddebo.mytimes.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.news.droiddebo.mytimes.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String NEWS_API_KEY = "33f337ecd72c443f9c4ec40e2076f873";
+}
---
+++ app/build/generated/source/buildConfig/debug/com/news/droiddebo/mytimes/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.news.droiddebo.mytimes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.news.droiddebo.mytimes";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "2.0";
+  // Field from build type: debug
+  public static final String NEWS_API_KEY = "33f337ecd72c443f9c4ec40e2076f873";
+}
